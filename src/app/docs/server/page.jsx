8f2b23253678bc6a62939042a136c9d9272dbb04
paragrghoshal <ghoshsar000@students.centergrove.k12.in.us>
2025-04-25 18:17:12 -0400

    added mini and started server

---
+++ src/app/docs/server/page.jsx
@@ -0,0 +1,9 @@
+import Server from "@/app/docs/mdx/server.mdx";
+
+export default function Docs(){
+    return (
+            <div className="background">
+                <Server />
+            </div>
+        );
+}
